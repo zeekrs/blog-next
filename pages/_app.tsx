9627dba40ecfb52d0496489c9f61d62512521774
zeekrs <admin@zeekrs.dev>
2021-07-26 14:56:03 +0800

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 
+console.log("test")
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
 }
